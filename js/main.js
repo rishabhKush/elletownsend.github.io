ded34b4e8977f03c95a3c8ed951c025d17dd55c1
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2019-10-09 14:39:56 +0100

    responsive nav bar

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function hideShowNav() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
