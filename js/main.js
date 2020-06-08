02071abe85cb5f43b348f7fe853f55505dda7e37
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2020-06-08 12:31:44 +0100

    site under construction, add filler page

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function hideShowNav() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
