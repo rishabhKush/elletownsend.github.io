edaaf68aca5dd0546c0e0c5a6ac6a3df421893e2
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2020-07-24 12:34:47 +0100

    other changes

--- js/main.js
+++ js/main.js
@@ -46,5 +46,4 @@ function toggleDarkMode() {
     var body = document.getElementById("body");
     var currentClass = body.className;
     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-    body.style.transition = "";
 }
